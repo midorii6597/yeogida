608bea875089797fcb22b202492f5fe29ef42479
Riny <add6847@naver.com>
2024-12-09 00:23:36 +0900

    서린-콘솔수정3

--- src/components/Bell.jsx
+++ src/components/Bell.jsx
@@ -148,8 +148,7 @@ const Bell = () => {
     const [hovered, setHovered] = useState(false);
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const [notifications, setNotifications] = useState([]);
-    const { user } = useAuth(); // AuthContext에서 user 객체 가져오기
-    const userId = user?.id; // user 객체에서 id 추출
+    const { user_id } = useAuth();
     const [friendRequests, setFriendRequests] = useState([]);
     const [friendRequestCount, setFriendRequestCount] = useState(0);
 
@@ -158,16 +157,18 @@ const Bell = () => {
     };
 
     useEffect(() => {
+        console.log('사용자 아이디:', user_id); // user_id 출력
+        
         const loadNotifications = async () => {
-            if (!userId) return; 
-            const alarms = await getUserAlarms(userId);
+            if (!user_id) return; 
+            const alarms = await getUserAlarms(user_id);
             if (alarms) {
                 setNotifications(alarms); 
             }
         };
 
         const loadFriendRequests = async () => {
-            if (!userId) return; 
+            if (!user_id) return; 
             try {
                 const requests = await getFriendRequest(); // 친구 요청 목록 조회
                 setFriendRequests(requests); 
@@ -179,7 +180,7 @@ const Bell = () => {
 
         loadNotifications();
         loadFriendRequests(); // 친구 요청 목록도 함께 로드
-    }, [userId]); // userId가 변경될 때마다 다시 호출
+    }, [user_id]); // userId가 변경될 때마다 다시 호출
 
     useEffect(() => {
         let timeout;
